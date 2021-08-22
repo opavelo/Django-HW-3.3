145744fa801766b4b5b48edd1fd17f3489c498ea
opavelo <opavelo@gmail.com>
2021-08-22 22:05:50 +0300

    first try

---
+++ api_with_restrictions/advertisements/views.py
@@ -0,0 +1,35 @@
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.viewsets import ModelViewSet
+
+from advertisements.filters import AdvertisementFilter
+from advertisements.models import Advertisement
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
+from advertisements.serializers import AdvertisementSerializer
+
+
+class AdvertisementViewSet(ModelViewSet):
+    """ViewSet для объявлений."""
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['creator']
+    # throttle_classes = [AnonRateThrottle, UserRateThrottle] # Указал в settings
+
+    def perform_create(self, serializer):
+        serializer.save(creator=self.request.user)
+
+    # TODO: настройте ViewSet, укажите атрибуты для кверисета,
+    #   сериализаторов и фильтров
+    def get_permissions(self):
+        """Получение прав для действий."""
+        if self.action in ["create"]:
+            return [IsAuthenticated()]
+        elif self.action in ["update", "destroy", "partial_update"]:
+            return [IsAdminUser()]
+        return []
+
+
+
+
+
